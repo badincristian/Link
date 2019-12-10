a400183e0aed802379f16b4b1be8456afaac8606
JavaScript <javascript@Admins-iMac-3.local>
2019-12-10 20:47:53 +0200

    ok

---
+++ JavaScript/first.js
@@ -0,0 +1,25 @@
+var name = "Cristian";
+var message = "Hello " + name + "!"; 
+var timeout = 3000; 
+var isNight = false; 
+var isMorning = false;
+var nothing = null;
+var date = new Date();
+var hour = date.getHours();
+
+if(hour >= 20 || hour <= 6) {
+    alert("Noapte Buna!");
+} else if (hour == 0) {
+    alert("Happy new year!")
+} else if(hour <=11) {
+    alert("Buna dimineata!");
+} else if (hour<=17) {
+    alert("Buna ziua!");
+} else {
+    alert("Buna seara!");
+}
+setTimeout(sayHello, timeout)
+function sayHello() {
+    //    alert(message);
+    console.log(message);
+}
