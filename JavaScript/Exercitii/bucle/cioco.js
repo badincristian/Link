c0804086b9f418d39bd7721a87ce010f3eed3faa
JavaScript <javascript@Admins-iMac-3.local>
2019-12-12 20:17:14 +0200

    lost

---
+++ JavaScript/Exercitii/bucle/cioco.js
@@ -0,0 +1,16 @@
+var money = 5;
+var chocolates = 0;
+var price = 1;
+
+do {     
+   money -= price;
+   chocolates++;
+} while(money >= price) {
+
+}
+
+document.write("Am cumparat " + chocolates + " ciocolate");
+document.write ("</br>");
+document.write("Am cumparat " + money + " lei");
+
+ 
